b225ec8bb2a5ff06eb3323c0318c60774485d957
Jhoel Perez <jhoel.jjpr@gmail.com>
2022-02-08 06:09:46 +0300

    fixed clock
    fixed pomodoros cycles 
    work cycle 25 min 
    short brake 5 min 
    long brake 5 min

--- pomodoro_GUI/main.py
+++ pomodoro_GUI/main.py
@@ -5,28 +5,55 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 0.1
+SHORT_BREAK_MIN = 0.1
 LONG_BREAK_MIN = 20
 
+COUNT = 10
+REPS = 0
+
 from math import floor
+from tabnanny import check
 from tkinter import *
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(COUNT*60)
+    global REPS
+    #work 25 > 5 min brake (4 times)
+    #20 min brake 
+    #change title accordingly
+    print(REPS)
+    #if last rep 8th cycle 
+    if REPS==7:
+        main_title.config(text="Long Brake",fg=GREEN)
+        count_down(LONG_BREAK_MIN*60)
+    #work 25
+    elif REPS%2==0:
+        main_title.config(text="Work Time",fg=RED)
+        count_down(WORK_MIN*60)
+    else:
+        #five minute brake
+        main_title.config(text="Short Brake",fg=GREEN)
+        count_down(SHORT_BREAK_MIN*60)
+    REPS+=1
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    min = floor(count/60)
-    seconds = count%60 if (count%60)>9 else f"0{count%60}"
-    canvas.itemconfig(timer_text,text=f"{min}:{seconds}")
-    window.after(1000,count_down,count-1)
+    if count >= 0:
+        min = floor(count/60)
+        seconds = count%60 if (count%60)>9 else f"0{count%60}"
+        canvas.itemconfig(timer_text,text=f"{min}:{seconds}")
+        window.after(1000,count_down,count-1)
+    else:
+        if REPS%2 != 0: 
+            tmp_mark = str(check_mark.cget("text")) + mark
+            check_mark.config(text = tmp_mark)
+        if REPS<8:
+            start_timer()
 # ---------------------------- UI SETUP ------------------------------- #
 FONT = ("Arial",50,"bold")
 LETTER_FONT = ("Arial",20,"normal")
-COUNT = 10
 
 window = Tk()
 window.title("Pomodoro GUI")
@@ -50,7 +77,7 @@ reset_b = Button(text="Reset",bg=YELLOW,highlightthickness=0,highlightbackground
 reset_b.grid(column=2,row=2)
 #check mark 
 mark = "✔"
-check_mark = Label(text=f"{mark}",bg=YELLOW,fg=RED,highlightthickness=0)
+check_mark = Label(text="",bg=YELLOW,fg=RED,highlightthickness=0)
 check_mark.grid(column=1,row=3)
 
 window.mainloop()
