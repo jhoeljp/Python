5c96c2e5906fb1edbb899e5b84e0e856f4a8d717
Jhoel Perez Ruiz <jhoel.jjpr@gmail.com>
2022-11-30 22:59:42 +0300

    get interent speed and testing twitter login

--- Twitter_Complain_Bot/complain_bot.py
+++ Twitter_Complain_Bot/complain_bot.py
@@ -6,6 +6,7 @@ from time import sleep
 #selenium dependencies 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class InternetSpeedTwitterBot():
 
@@ -16,40 +17,88 @@ class InternetSpeedTwitterBot():
 
     def get_internet_speed(self):
 
+        #go to internet speed test webpage
         self.driver.get("https://www.speedtest.net/")
 
         sleep(2)
 
+        #start Internet speed test
         GO_btn = self.driver.find_element(By.CLASS_NAME,"start-text")
         GO_btn.click()
 
-        # ISP_name = self.driver.find_element(By.CLASS_NAME,"hostUrl")
-        # print(ISP_name.text)
-
         sleep(60)
 
+        #Get Internet Speed Info
         Download_speed = self.driver.find_element(By.CSS_SELECTOR,"span.download-speed").text
         Upload_speed = self.driver.find_element(By.CSS_SELECTOR,"span.upload-speed").text
         ISP_Name = self.driver.find_element(By.CLASS_NAME,"js-data-sponsor").text
+        ISP_accronym = self.driver.find_element(By.CLASS_NAME,"js-data-isp").text
 
         sleep(2)
-        print(ISP_Name)
-        print(f"Download Speed:{Download_speed}\nUpload Speed:{Upload_speed}")
+        Dict = {}
+        Dict['ISP_Name'] = ISP_Name
+        Dict['Download']=Download_speed
+        Dict['Upload']=Upload_speed
 
-    def tweet_at_provider():
-        pass
+        #close driver
+        self.driver.close()
+
+        return Dict
+
+    def tweet_at_provider(self, message, actual_speed):
+        #get authorization credentials
+        self.authentication_credentials()
+
+        #check if internet is underperforming
+        if actual_speed < self.INTERNET_IDEAL:
+
+            message += str(self.INTERNET_IDEAL) + " Mbs down/up !!!"
+
+            print(message)
+
+            #log in with credentials 
+            self.driver.get("https://twitter.com/i/flow/login")
+
+            sleep(2)
+
+            #input user credentials
+            user = self.driver.find_element(By.CSS_SELECTOR,"input.r-13qz1uu")
+            user.send_keys(self.TWITTER_USER)
+
+            user.send_keys(Keys.ENTER)
+
+            sleep(3)
+
+            #input password credentials 
+
+
+            #write out new tweet 
+
+            #send tweeet 
+
+            #close driver
+            self.driver.close()
+
+
+
+        else:
+            print("Internet Speed not slow enough to complain! ")
 
     def authentication_credentials(self):
 
+        #build environment path
         env_path = f"{getcwd()}\secrets.env"
 
         if exists(env_path):
+            #load environemnt path 
             load_dotenv(env_path)
 
+            #get twitter credentials for login
             self.TWITTER_USER = environ.get("TWITTER_USER")
             self.TWITTER_PASSWORD = environ.get("TWITTER_PASSWORD")
             self.INTERNET_IDEAL = int(environ.get("INTERNET_IDEAL"))
 
         else:
+            #incorrect execution path
             print("Incorrect execution path or secrets.env file missing! ")
             print(env_path)
--- Twitter_Complain_Bot/main.py
+++ Twitter_Complain_Bot/main.py
@@ -6,6 +6,18 @@ if __name__ == "__main__":
     bot = InternetSpeedTwitterBot()
 
     try:
-        bot.get_internet_speed()
+        #get internet speed and ISP 
+        info = bot.get_internet_speed()
+
+        #compose messsage to tweet 
+        Complain = f'''Hello {info['ISP_Name']}, why is my interent speed 
+        {info['Download']}down/{info["Upload"]}up when I am paying for '''
+
+        bot.tweet_at_provider("",100)
+
+        #login and send tweet
+
+
+
     except Exception as ex:
         print(str(ex))
