949adb0fc7d5bae83e9da79903fc45aac679c7f0
Jhoel Perez Ruiz <jhoel.jjpr@gmail.com>
2022-12-02 20:31:33 +0300

    implemented pop-up scroll down

--- Instagram_Follower_Bot/follower.py
+++ Instagram_Follower_Bot/follower.py
@@ -22,6 +22,7 @@ class Instagram_Follower():
 
         #login on instagram 
         self.driver.get(self.URL)
+        self.driver.maximize_window()
 
         sleep(3)
 
@@ -35,7 +36,7 @@ class Instagram_Follower():
         #send login in credentials
         insta_password.send_keys(Keys.ENTER)
 
-        sleep(4)
+        sleep(7)
         # sleep(400)
         #dismiss notification pop-up 
         self.driver.find_element(By.CSS_SELECTOR,"button._a9_1").click()
@@ -68,10 +69,32 @@ class Instagram_Follower():
         #got to target account 
         self.driver.get(self.URL+'/' + self.TARGET_ACCOUNT)
 
-        sleep(4)
+        sleep(6)
         
+        #go to the followers section
+        page = self.driver.find_element(By.XPATH,"/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[2]/a")
+        page.click()
 
-        sleep(400)
-        pass
+        sleep(4)
+        #locate body of pop-up page
+        follower_body = self.driver.find_element(By.XPATH,"/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]")
+
+        #scroll continously or for a set range
+        #scroll 25 times with 8 accounta a page
+        scroll = 0
+
+        while scroll < 100:
+
+            #scroll down every 5 seconds 
+            self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', follower_body)
+            
+            sleep(5)
+            scroll += 1
+
+        #terminate selenium driver 
+        self.driver.close()
+
+        self.driver.quit()
+
     def follow(self):
         pass
